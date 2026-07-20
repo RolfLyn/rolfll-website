24c291f57f47ae7218740fca4f1e5af4088da762
RolfLyn <rolf@rolfll.com>
2026-07-20 22:13:45 +0200

    Set real AAU academic profile URL

--- src/data/social.ts
+++ src/data/social.ts
@@ -1,5 +1,4 @@
-// Replace AAU_PROFILE_URL with your real AAU VBN research-portal profile URL before deploying.
-export const AAU_PROFILE_URL = 'https://vbn.aau.dk/en/persons/REPLACE_WITH_YOUR_PROFILE_SLUG';
+export const AAU_PROFILE_URL = 'https://vbn.aau.dk/da/persons/rolfll/';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/starpen/';
 export const GITHUB_URL = 'https://github.com/RolfLund';
 export const EMAIL = 'rolf@rolfll.com';
